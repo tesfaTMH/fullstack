6796044a92200702d2b82c66fc3d2c7ce80ccdd2
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2023-11-08 21:15:10 +0200

    solution to 2.1-2.5 courseinfo step6-step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -16,20 +16,14 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />
-    <Part part={parts[3]}
-    />      
-  </>
+const Content = ({ parts }) => {
+  const partComponent = []
+  for (let i=0; i < parts.length; i++){
+    partComponent.push(<Part part={parts[i]} key={parts[i].id} />)
+  }
+  return partComponent
+}
+  
 
 const Course = ({ course }) => 
   <>
@@ -42,37 +36,61 @@ const Course = ({ course }) =>
   </>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 3
-      }
-    ]
-  }
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
-      <Total parts={course.parts} />
+      <>
+        <Course course={course[0]} />
+        <Total parts={course[0].parts} />
+      </>
+      <>
+        <Course course={course[1]} />
+        <Total parts={course[1].parts} />
+      </>
     </>
   )
 }
