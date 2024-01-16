efcd4a962b59bb9b17216744c5df001926bc22a6
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-01-16 16:44:51 +0200

    Solution to ex3.14 post updated for updating phonebook information on MongoDB

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 //middleware for defining enviroment variables 
 require('dotenv').config()
 
-const People = require('./models/people')
+const Person = require('./models/person')
+const person = require('./models/person')
 
 app = express()
 
@@ -30,7 +31,7 @@ app.use(morgan(':method :host :url :status :res[content-length] - :response-time
 //  return req.params[param]
 //})
 
-const persons = [
+{/*const persons = [
     { 
       id: 1,
       name: "Arto Hellas", 
@@ -52,9 +53,18 @@ const persons = [
       number: "39-23-6423122"
     }
 ]
+*/}
 
-app.get('/api/persons', (req, res) => {
+{/*app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+*/}
+
+// get all phonebook information from MongoDB
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -65,7 +75,7 @@ app.get('/info', (req, res) => {
         `)
 })
 
-app.get('/api/persons/:id', (req, res) => {
+{/*app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
   
@@ -75,6 +85,23 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end()
   }
 
+})
+*/}
+
+// Find by id using data from MongoDB
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person){
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -88,7 +115,7 @@ const randomId = () => {
   return Math.floor(Math.random()) + persons.length
 }
 
-app.post('/api/persons', (req, res) => {
+{/*app.post('/api/persons', (req, res) => {
   const body = req.body
 
   const found = persons.find(person => person.name === body.name)
@@ -113,6 +140,25 @@ app.post('/api/persons', (req, res) => {
   persons = persons.concat(person)
   res.json(persons)
 
+})
+*/}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (body.name === undefined || body.number === undefined){
+    return res.status(400).json({ error: 'Incomplete person information either name or number is missing'})
+  }
+
+  const person = new Person({
+    id: randomId(),
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 const PORT = process.env.PORT
