0090f65897285fa6cd8268198b1ddc8a8493fc87
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-01-18 14:24:48 +0200

    possible errors defined

--- nodejs_intro/notes-backend/index.js
+++ nodejs_intro/notes-backend/index.js
@@ -52,11 +52,11 @@ app.use(requestLogger)
       important: true
     }
   ]
-*/}
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
+*/}
 
 app.get('/api/notes', (req, res) => {
     Note.find({}).then(notes => {
